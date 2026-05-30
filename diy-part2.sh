daba69b6d38948ed11a39281e3d67119be655f6e
zyq7885696 <59403703+zyq7885696@users.noreply.github.com>
2026-05-30 16:48:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
 
-# 1. 默认主题为 Argon
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 修改openwrt登陆地址,把下面的 192.168.10.1 修改成你想要的就可以了
+#sed -i 's/192.168.100.1/172.16.1.251/g' package/base-files/files/bin/config_generate
 
-# 2. 关闭广告、VPN、多余插件（保持纯净）
-sed -i '/autosamba/d' .config
-sed -i '/ddns/d' .config
-sed -i '/qemu/d' .config
-sed -i '/dockerman/d' .config
+# 修改主机名字，把 iStore OS 修改你喜欢的就行（不能纯数字或者使用中文）
+# sed -i 's/OpenWrt/iStore OS/g' package/base-files/files/bin/config_generate
 
-# 3. 时区改为上海
-sed -i "s/'UTC'/'Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+# ttyd 自动登录
+# sed -i "s?/bin/login?/usr/libexec/login.sh?g" ${GITHUB_WORKSPACE}/openwrt/package/feeds/packages/ttyd/files/ttyd.config
+
+# 添加自定义软件包
+# echo '
+# CONFIG_PACKAGE_luci-app-mosdns=y
+# CONFIG_PACKAGE_luci-app-zerotier=y
+# CONFIG_PACKAGE_luci-app-openclash=y
+# CONFIG_PACKAGE_luci-app-diskman=y
+# ' >> .config

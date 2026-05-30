d21e31f41b5b56238b50901908488fc6336d2502
zyq7885696 <59403703+zyq7885696@users.noreply.github.com>
2026-05-30 12:03:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,5 +3,4 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # 额外添加 iStore 官方源（确保能编译）
-echo >> feeds.conf.default
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default

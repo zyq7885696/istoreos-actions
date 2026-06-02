e37b92152eb57a7bb09b64b0294d449cd26cdd88
zyq7885696 <59403703+zyq7885696@users.noreply.github.com>
2026-06-02 09:19:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # 修改openwrt登陆地址,把下面的 192.168.10.1 修改成你想要的就可以了
-#sed -i 's/192.168.100.1/172.16.1.251/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.100.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把 iStore OS 修改你喜欢的就行（不能纯数字或者使用中文）
 # sed -i 's/OpenWrt/iStore OS/g' package/base-files/files/bin/config_generate
@@ -22,7 +22,6 @@
 # 添加自定义软件包
 # echo '
 # CONFIG_PACKAGE_luci-app-mosdns=y
-# CONFIG_PACKAGE_luci-app-zerotier=y
+# CONFIG_PACKAGE_luci-app-adguardhome=y
 # CONFIG_PACKAGE_luci-app-openclash=y
-# CONFIG_PACKAGE_luci-app-diskman=y
 # ' >> .config
